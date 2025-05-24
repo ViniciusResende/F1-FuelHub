5fcd9388256806c897b7badeb0294e361ac2c6f9
Vinicius Faria <viniciusfariaresende@gmail.com>
2025-05-24 09:55:12 -0300

    feat: adding notFound middleware

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,15 +1,16 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import routes from "./routes";
-import { errorHandler } from "./middlewares/error.middleware";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import routes from './routes';
+import { notFound, errorHandler } from './middlewares/error.middleware';
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.use("/api", routes);
+app.use('/api', routes);
 
-app.use(errorHandler);           // must be last
+app.use(notFound);
+app.use(errorHandler); // must be last
