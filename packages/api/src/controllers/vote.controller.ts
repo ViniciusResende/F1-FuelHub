d330f7373bd715a455fb8579a9949f402aa17403
Vinicius Faria <viniciusfariaresende@gmail.com>
2025-05-25 08:06:11 -0300

    feat: added new team voting apis

---
+++ packages/api/src/controllers/vote.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { HttpError } from '../middlewares/error.middleware';
+import { upsertVote, leaderboard } from '../services/vote.service';
+
+export async function postVote(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { email, team } = req.body;
+    if (!email || !team) throw new HttpError(400, 'email and team required');
+
+    await upsertVote(String(email), String(team));
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getLeaderboard(
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const data = await leaderboard();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+}
