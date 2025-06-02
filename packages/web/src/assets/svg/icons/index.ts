a3bc84474885015c310fae7434b746c48269160c
Vinicius Faria <viniciusfariaresende@gmail.com>
2025-06-02 10:28:19 -0300

    feat: added placement medal icons

---
+++ packages/web/src/assets/svg/icons/index.ts
@@ -0,0 +1,7 @@
+/** Imports */
+import FirstPlaceMedalIcon from './first-place-medal.svg';
+import SecondPlaceMedalIcon from './second-place-medal.svg';
+import ThirdPlaceMedalIcon from './third-place-medal.svg';
+
+/** Exports */
+export { FirstPlaceMedalIcon, SecondPlaceMedalIcon, ThirdPlaceMedalIcon };
